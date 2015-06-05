fe2061f2cd2846dd4a80bab4b343609ea180f3f8
pkmnnerd <b_liu72@yahoo.com>
2015-06-05 12:01:26 -0400

    replace urllib2 with requests

--- pathway/kegg.py
+++ pathway/kegg.py
@@ -29,11 +29,11 @@ def search(args):
     text = r.text;
 
     # Gets the text from the url
-    try:
-        response = urllib2.urlopen(url)
-        text = response.read()
-    except Exception:
-        raise ValueError("Bad URL: " + url)
+ #   try:
+#        response = urllib2.urlopen(url)
+ #       text = response.read()
+  #  except Exception:
+   #     raise ValueError("Bad URL: " + url)
     data = {}
 
     # This part handles the find, list, link, and conv operations as
