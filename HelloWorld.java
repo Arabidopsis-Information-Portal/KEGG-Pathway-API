ae1172a0f0fcc28a97003d979f614e29a5457dc7
pkmnnerd <b_liu72@yahoo.com>
2015-06-10 14:33:07 -0400

    organized git repo

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-
-	for (int i = 1; i <= 5; i++)
-	    System.out.println("Hello world " + i + "!");
-	System.out.println("Goodbye.");
-
-    }
-}
