9fc2aa448ad7f8e4c037a9b0b5f50baffe157a56
pkmnnerd <b_liu72@yahoo.com>
2015-06-11 10:25:55 -0400

    updated listing pathways

--- services/pathway/main.py
+++ services/pathway/main.py
@@ -1,13 +1,32 @@
 import requests
 import json
-import services.common.vars
+import services.common.vars as vars
 
 def search(args):
     if 'identifier' in args.keys():
         url = vars.url + 'list/' + args['identifier']
         r = requests.get(url)
         if r.status_code != 200:
-            raise Exception("Can't access url")
-        text = t.text;
+            raise Exception("Can't access url" + url)
+        text = r.text
+
     else:
-        url = vars.url + list
+        url = vars.url + 'list/pathway/ath'
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise Exception("Can't access url" + url)
+        text = r.text
+
+        data = {}
+        # Splits the received text into an array of lines
+        lines = text.split('\n')
+        for line in lines:     
+            # splits each line into two parts by the tab delimiter with the
+            # first part being the key and the second part being the value
+            # in the returned JSON
+            parts = line.split(vars.delimiter, 1);
+            if len(parts) == 2:
+                data[parts[0]] = parts[1]
+
+        
+    print json.dumps(data)
