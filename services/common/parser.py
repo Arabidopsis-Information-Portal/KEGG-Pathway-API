d306cc57e0624478622ba9a4080a02eb0948389c
pkmnnerd <b_liu72@yahoo.com>
2015-06-22 16:45:00 -0400

    Added accessing a field of a pathway back to API.

--- services/common/parser.py
+++ services/common/parser.py
@@ -7,12 +7,20 @@ def parse(data, field):
         result = {field.lower(): data[0]}
         return result
 
-    if field in {"GENE", "COMPOUND", "MODULE", "DISEASE", "DRUG"}:
+    if field == 'GENE':
         arr = []
         for line in data:
             parts = line.split(None, 1)
-            gene = {'id':parts[0], field.lower():parts[1]}
+            gene = {'locus':parts[0], field.lower():parts[1]}
             arr.append(gene)
+        return arr
+
+    if field in {"COMPOUND", "MODULE"}:
+        arr = []
+        for line in data:
+            parts = line.split(None, 1)
+            entry = {'id':parts[0], field.lower():parts[1]}
+            arr.append(entry)
         return arr
 
     if field == "DBLINKS":
