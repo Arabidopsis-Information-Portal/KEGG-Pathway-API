c431ef771becbeb6e5e7d635d36a3b7ffd8653bb
pkmnnerd <b_liu72@yahoo.com>
2015-07-20 15:42:20 -0400

    The problem is that the API times out, but when the raise Exception statment is put back in, the program successfully reaches there and raises an exception

--- services/genes_by_kegg_pathway/main.py
+++ services/genes_by_kegg_pathway/main.py
@@ -93,7 +93,7 @@ def search(args):
             #print json.dumps(element)
             #print '---'
 
-        raise Exception('here')
+        #raise Exception('here')
         print json.dumps({'hello':'test'})
         return
     else:
