b2137ba60aeaaaceb7f48d337282354ea5502ba6
pkmnnerd <b_liu72@yahoo.com>
2015-07-21 15:18:32 -0400

    added orthology to valid fields

--- services/common/vars.py
+++ services/common/vars.py
@@ -5,4 +5,4 @@ list = 'list' # Lists the entries in a database
 get = 'get'   # Returns the given database entry and its associated data
 delimiter = '\t'
 fields = {"name", "description", "class", "module", "disease", "drug", \
-"dblinks", "organism", "gene", "compound", "ko_pathway", "reference"}
+"dblinks", "organism", "gene", "compound", "ko_pathway", "reference", "orthology"}
